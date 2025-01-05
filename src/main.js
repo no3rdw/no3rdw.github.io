15ee9639f5b8365dae3d08a6b389370b9a0cb3d3
Paul Gallo <pgallo@gmail.com>
2025-01-05 00:42:21 -0500

    swipe added

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 //import "../node_modules/bootstrap/dist/js/bootstrap.js";
 
 import Alpine from 'alpinejs';
+import swipePlugin from "alpinejs-swipe";
 import flickr from './flickr.js';
-
 
 Alpine.data('flickr', flickr);
 
 window.Alpine = Alpine
+Alpine.plugin(swipePlugin);
 Alpine.start()
